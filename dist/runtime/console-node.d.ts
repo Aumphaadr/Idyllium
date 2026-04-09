7258b4bfc75c9104c47faa10b318a97950725150
Aumphaadr <129282307+Aumphaadr@users.noreply.github.com>
2026-04-10 01:07:19 +0500

    Delete dist directory

--- dist/runtime/console-node.d.ts
+++
@@ -1,5 +0,0 @@
-import { ConsoleIO } from './runtime';
-import * as readline from 'readline';
-export declare function createNodeConsole(): ConsoleIO;
-export declare function closeNodeConsole(rl: readline.Interface): void;
-//# sourceMappingURL=console-node.d.ts.map
